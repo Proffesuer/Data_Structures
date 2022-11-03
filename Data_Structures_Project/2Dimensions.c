24429db5fc800b5be447668bf7cd7564c2f26537
Proffesuer <levischisira0@gmail.com>
2022-11-03 23:44:40 +0300

    Initialized variables and a loop to allow enter data in an array

--- Data_Structures_Project/2Dimensions.c
+++ Data_Structures_Project/2Dimensions.c
@@ -3,6 +3,36 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    /* 2D array declaration */
+
+    int table[2][3];
+    /* other variables for loops*/
+    int i,j;
+
+    for(i=0;i<2;i++){
+    for(j=0;j<3;j++){
+
+    printf("Enter the values of Table[%d][%d]:",i,j);
+    scanf("%d",&table[i][j]);
+
+
+
+
+
+
+    }
+
+
+
+
+
+    }
+
+
+
+
+
+
     return 0;
 }
